937ec0804d6b3edea3d337ea65990753a96bd21e
Yukitkym <yukitkym1011@gmail.com>
2022-03-29 11:34:09 +0900

    Create addFunction and deleteFunction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.id = "li-tag";
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className = "todo-name";
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    const deleteTarget = deleteButton.closest("#li-tag");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
